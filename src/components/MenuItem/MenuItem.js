730cc352f15876abdf133d654a5c9897efb52287
Azu <abbey-1825@hotmail.com>
2020-10-26 13:46:09 -0300

    add withRouter + destructure menu item props

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, image }) => (
-  <div className="menu-item">
-    <div className="background-image" style={{ backgroundImage: `url(${image})` }} />
+const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="content">
       <h1 className="title">{title}</h1>
       <p className="subtitle">SHOP NOW</p>
@@ -10,4 +11,4 @@ const MenuItem = ({ title, image }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
