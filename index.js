a1ed4584206cdb665e9c9805d2175b5fc521a07e
Azu <abbey-1825@hotmail.com>
2020-11-23 19:41:11 -0300

    add redux persist to persist cart

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App.js';
 import './style.scss';
 
-import store from 'data/store';
+import { store, persistor } from 'data/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById('root')
