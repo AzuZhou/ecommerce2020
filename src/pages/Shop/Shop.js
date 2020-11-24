d133f0f383291b7d4ed3bc32ce769797464c3b6e
Azu <abbey-1825@hotmail.com>
2020-11-24 15:29:05 -0300

    add collection page and add subroutes for each collection

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Collection from 'pages/Collection';
 import CollectionsOverview from 'components/CollectionsOverview';
 
-const Shop = () => (
-  <div className='shop-page'>
-    <CollectionsOverview />
-  </div>
-);
+const Shop = ({ match }) =>
+  console.log('MATCH', match) || (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={Collection} />
+    </div>
+  );
 
 export default Shop;
