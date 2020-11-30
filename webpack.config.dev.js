c6234c6cbf5ba4cb34c9b5db8982e2fe038dcfe7
Azu <abbey-1825@hotmail.com>
2020-11-30 15:24:37 -0300

    remove redux logs in production mode

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,49 +1,66 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const fs = require('fs');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: [path.resolve(__dirname, './index.js')],
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, './'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: true,
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.[s]css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      assets: path.resolve(__dirname, 'src/assets/'),
-      components: path.resolve(__dirname, 'src/components/'),
-      pages: path.resolve(__dirname, 'src/pages/'),
-      utils: path.resolve(__dirname, 'src/utils/'),
-      data: path.resolve(__dirname, 'src/data/'),
+module.exports = env => {
+  const currentPath = path.join(__dirname);
+  const basePath = currentPath + '/.env';
+  const envPath = basePath + '.' + env.ENVIRONMENT;
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
+  return {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: [path.resolve(__dirname, './index.js')],
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, './'),
+      publicPath: '/',
+      filename: 'bundle.js',
     },
-  },
+    devServer: {
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: true,
+      }),
+      new webpack.DefinePlugin(envKeys),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.[s]css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        assets: path.resolve(__dirname, 'src/assets/'),
+        components: path.resolve(__dirname, 'src/components/'),
+        pages: path.resolve(__dirname, 'src/pages/'),
+        utils: path.resolve(__dirname, 'src/utils/'),
+        data: path.resolve(__dirname, 'src/data/'),
+      },
+    },
+  };
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,43 +2,57 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: [path.resolve(__dirname, './index.js')],
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, './'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: true,
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.[s]css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      components: path.resolve(__dirname, 'src/components/'),
-      pages: path.resolve(__dirname, 'src/pages/'),
-      assets: path.resolve(__dirname, 'src/assets/'),
-      utils: path.resolve(__dirname, 'src/utils/'),
-      data: path.resolve(__dirname, 'src/data/'),
+module.exports = env => {
+  const currentPath = path.join(__dirname);
+  const basePath = currentPath + '/.env';
+  const envPath = basePath + '.' + env.ENVIRONMENT;
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+
+  return {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: [path.resolve(__dirname, './index.js')],
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, './'),
+      publicPath: '/',
+      filename: 'bundle.js',
     },
-  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: true,
+      }),
+      new MiniCssExtractPlugin(),
+      new webpack.DefinePlugin(envKeys),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.[s]css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components/'),
+        pages: path.resolve(__dirname, 'src/pages/'),
+        assets: path.resolve(__dirname, 'src/assets/'),
+        utils: path.resolve(__dirname, 'src/utils/'),
+        data: path.resolve(__dirname, 'src/data/'),
+      },
+    },
+  };
 };
