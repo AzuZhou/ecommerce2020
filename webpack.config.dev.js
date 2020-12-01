3be3acec56a6c6ee9127ff58abd7953f0357b59f
Azu <abbey-1825@hotmail.com>
2020-12-01 12:15:58 -0300

    update webpack, webpack-cli and webpack-dev-server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 module.exports = env => {
   const currentPath = path.join(__dirname);
   const basePath = currentPath + '/.env';
-  const envPath = basePath + '.' + env.ENVIRONMENT;
+  const envPath = basePath + '.' + env;
   const finalPath = fs.existsSync(envPath) ? envPath : basePath;
   const fileEnv = dotenv.config({ path: finalPath }).parsed;
 
